18bca57f63dd2b4a1621ba6cbffc9071f5e46b3e
Quincy Thai <quincythai50@icloud.com>
2023-10-13 18:00:55 -0700

    Add files

---
+++ week-5/vite-project/src/App.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/about",
+    element: <div>About</div>,
+  },
+]);
+
+
+function App() {
+
+  const [image, setImage] = useState("Fetching image of dog...");
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => setImage(data.message));
+  }, []);
+
+  return (
+    <>
+      <img src={image} alt="photo of a dog" width={500} />
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
